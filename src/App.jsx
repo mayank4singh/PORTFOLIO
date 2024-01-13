36583d5608e211d09b8dd14de0c91c356ceda0de
Mayank Singh <117936126+mayank4singh@users.noreply.github.com>
2024-01-13 05:46:25 +0000

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mayank Singh",
+  title: "App Developer",
+  email: "mailatmausam@gmail.com",
+  gitHub: "mayank4singh",
+  instagram: "_thamayanksinghna_",
+  linkedIn: "mayank4singh",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
