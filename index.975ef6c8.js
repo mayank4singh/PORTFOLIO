7a1f42a6c9ccea0258240434a5878e4d092a41f4
Mayank Singh <117936126+mayank4singh@users.noreply.github.com>
2024-01-13 06:00:07 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28759,24 +28759,24 @@ const imageAltText = "desktop with books and laptop";
  * links section. Below is a sample, update to reflect links you'd like to highlight.
  */ const projectList = [
     {
-        title: "10 Things To Know About Azure Static Web Apps \uD83C\uDF89",
-        description: "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-        url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i"
+        title: "Crypt Me",
+        description: "An android app to encrypt and decrypt your text using cipher techniques",
+        url: "https://github.com/mayank4singh/Crypt_Me"
     },
     {
-        title: "Web Development for Beginners",
-        description: "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-        url: "https://github.com/microsoft/web-dev-for-beginners"
+        title: "Shorten URL",
+        description: "An android app to short your long URL using Firebase.",
+        url: "https://github.com/mayank4singh/ShortenURL"
     },
     {
-        title: "My Resume Site",
-        description: "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-        url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website"
+        title: "Coin Toss",
+        description: "An android app which uses animation to toss coin.",
+        url: "https://github.com/mayank4singh/CoinToss"
     },
     {
-        title: "GitHub Codespaces and github.dev",
-        description: "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-        url: "https://www.youtube.com/watch?v=c3hHhRME_XI"
+        title: "Face Detection",
+        description: "An Android app that detects how often you're smiling and if your eyes are open. It employs the notion of AI/ML.",
+        url: "https://github.com/mayank4singh/FaceDetection"
     }, 
 ];
 const Portfolio = ()=>{
